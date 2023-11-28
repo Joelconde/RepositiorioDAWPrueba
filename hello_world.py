2b0bfcea2a0277216acf6a459c3443157654cef2
Alumne 1r <joelrodriguezconde00@gmail.com>
2023-11-28 13:01:21 +0100

    Hemos vuelto a cambiar el texto

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 for i in range(0, 5):
-    print("EStare atento a las explicaciones para no preguntar lo mismo 20 veces")
+    print("Primero estar atento, luego copiar")
 
     # A esta primera linea se le llama shebang, indica como ejecutar el codigo sin necesidad de indicar el programa que lo interpreta
