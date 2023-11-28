6af707c73c2bc0f740a2aa6db154e7ef813e5ed3
Alumne 1r <joelrodriguezconde00@gmail.com>
2023-11-28 13:05:48 +0100

    Hemos pasado a 33 repeticiones

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-for i in range(0, 10):
+for i in range(0, 33):
     print("EStare atento a las explicaciones para no preguntar lo mismo 20 veces")
 
     # A esta primera linea se le llama shebang, indica como ejecutar el codigo sin necesidad de indicar el programa que lo interpreta
