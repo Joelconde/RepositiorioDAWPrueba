f0643eef4372cd6a7d105704cda287e0b88c85fe
Alumne 1r <joelrodriguezconde00@gmail.com>
2023-11-28 13:42:28 +0100

    como

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-for i in range(0, 33):
-    print("Primero estar atento, luego copiar")
+for i in range(0, 3):
+    print("Primero estar atento")
 
     # A esta primera linea se le llama shebang, indica como ejecutar el codigo sin necesidad de indicar el programa que lo interpreta
