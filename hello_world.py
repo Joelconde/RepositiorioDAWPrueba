9a87dad79d94442f3fac086886da092faf011b22
Alumne 1r <joelrodriguezconde00@gmail.com>
2023-11-28 13:41:26 +0100

    EL cambiado el rango

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-for i in range(0, 35):
+for i in range(0, 25):
     print("Voy a aprobar el ciclo de daw")
 
     # A esta primera linea se le llama shebang, indica como ejecutar el codigo sin necesidad de indicar el programa que lo interpreta
