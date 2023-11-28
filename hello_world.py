ff207b0241b0e5c43003f3db12d78c6b0bf416db
Alumne 1r <joelrodriguezconde00@gmail.com>
2023-11-28 12:16:48 +0100

    Hemos anyadido un fichero ejecutable

---
+++ hello_world.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+for i in range(0, 50):
+    print("No copiare en clase de Programacion")
+
+    # A esta primera linea se le llama shebang, indica como ejecutar el codigo sin necesidad de indicar el programa que lo interpreta
