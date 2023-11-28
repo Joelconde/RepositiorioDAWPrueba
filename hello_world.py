3f7b513bc20125554f097eab26bf10b0f8f32a93
Alumne 1r <joelrodriguezconde00@gmail.com>
2023-11-28 12:48:43 +0100

    Hemos cambiado el numero

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-for i in range(0, 50):
+for i in range(0, 5):
     print("No copiare en clase de Programacion")
 
     # A esta primera linea se le llama shebang, indica como ejecutar el codigo sin necesidad de indicar el programa que lo interpreta
